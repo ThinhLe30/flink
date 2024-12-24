24ecb3a0804b8eda84898ed3daa82ab9f24f549b
Thinh 'Central' Le <thinhmnsd2002@gmail.com>
2024-12-24 10:07:54 +0700

    feature: correct artifact id

--- flink-filesystems/flink-s3-fs-presto/deploy.sh
+++ flink-filesystems/flink-s3-fs-presto/deploy.sh
@@ -6,7 +6,7 @@ mvn deploy:deploy-file \                                                        
   -Dfile=flink-filesystems/flink-s3-fs-presto/target/flink-s3-fs-presto-1.18.0.jar \
   -Durl=https://artifacts.vtvlive.vn/repository/Giaitri_Maven_mixed/ \
   -DgroupId=org.apache.flink \
-  -DartifactId=flink-s3-fs-base \
+  -DartifactId=flink-s3-fs-presto \
   -Dversion=1.18.0-cellutions-1 \
   -Dpackaging=jar \
   -DrepositoryId=Giaitri_Maven_group
